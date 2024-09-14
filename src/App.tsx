2337404532cc1baafe453f762883c622854e3833
Charles Huang <charles6huang@gmail.com>
2024-09-14 14:57:50 -0400

    plant

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,26 @@ import { MapView, useMapData, useMap, Label } from "@mappedin/react-sdk";
 import "@mappedin/react-sdk/lib/esm/index.css";
 
 function MyCustomComponent() {
-	const { mapData } = useMap();
+	const { mapView, mapData } = useMap();
+
+	mapView.on("click", async (event) => {
+		if (event.models.length > 0) {
+			//If a 3D Model was clicked on, remove it.
+			mapView.Models.remove(event.models[0]);
+		} else {
+			//If no model was clicked on, add one at the click coordinate.
+			mapView.Models.add(
+				{
+					target: event.coordinate,
+					rotation: [90, 0, 0],
+					interactive: true,
+				},
+				{
+					url: "https://6ch8w7.csb.app/plant.glb",
+				}
+			);
+		}
+	});
 
 	return mapData.getByType("space").map((space) => {
 		return <Label target={space.center} text={space.name} />;
@@ -29,7 +48,10 @@ export default function App() {
 
 	return mapData ? (
 		<MapView mapData={mapData}>
-			<MyCustomComponent />
+			<div>
+				<MyCustomComponent />
+			</div>
+			<div></div>
 		</MapView>
 	) : null;
 }
